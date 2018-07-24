2f82d768c44b7043af3612261886b43a57c3fe9e
Sarthak Agrawal <saragrawal@DELC02WQ0RYHTD7.sea.corp.expecn.com>
2018-07-24 17:51:44 +0530

    First Commit

---
+++ src/main/java/com/expedia/www/hackathon/backpackersPool/BackpackersPoolApplication.java
@@ -0,0 +1,12 @@
+package com.expedia.www.hackathon.backpackersPool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpackersPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackpackersPoolApplication.class, args);
+	}
+}
---
+++ src/test/java/com/expedia/www/hackathon/backpackersPool/BackpackersPoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.expedia.www.hackathon.backpackersPool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackpackersPoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
