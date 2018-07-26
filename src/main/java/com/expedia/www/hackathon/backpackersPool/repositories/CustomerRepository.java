f6dbb01f70c5f65abbfbd54354ac67fab63c5b00
= <=>
2018-07-26 21:07:42 +0530

    h2 db is working

--- src/main/java/com/expedia/www/hackathon/backpackersPool/repositories/CustomerRepository.java
+++ src/main/java/com/expedia/www/hackathon/backpackersPool/repositories/CustomerRepository.java
@@ -3,10 +3,12 @@ package com.expedia.www.hackathon.backpackersPool.repositories;
 import com.expedia.www.hackathon.backpackersPool.models.Customer;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
+
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
-
+@Transactional
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 
 }
